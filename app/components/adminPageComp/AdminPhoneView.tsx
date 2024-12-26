582ba604a8f45a4fd6c99da16c2074c0a5fb20a6
muhammad subhan <muhammedsubhan692@gmail.com>
2024-12-26 07:26:55 +0500

    admin mobile view

--- app/components/adminPageComp/AdminPhoneView.tsx
+++ app/components/adminPageComp/AdminPhoneView.tsx
@@ -13,7 +13,28 @@ const AdminPhoneView = () => {
           </div>
         </div>
         {/* mobile  */}
-        <div></div>
+        <div className="flex justify-center items-center mt-16">
+          <div
+            style={{
+              boxShadow: "rgba(0, 0, 0, 0.2) 0px 20px 30px",
+            }}
+            className="border-[5.2px]  border-white rounded-e-[40px] rounded-s-[40px] h-[680px] w-[322px] "
+          >
+            <div className="bg-black h-full flex flex-col pt-20 rounded-e-[35px] rounded-s-[35px]">
+              <div>
+                <div className="flex flex-col items-center gap-3">
+                  <div className="rounded-full h-20 w-20 bg-white flex items-center justify-center">
+                    <p className="font-bold text-xl">M</p>
+                  </div>
+                  <div className="text-white font-semibold">
+                    <p>@MuhammadSubhan</p>
+                  </div>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
