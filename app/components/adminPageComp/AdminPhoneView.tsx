e25e9035c22691987424015f66c328f531060136
muhammad subhan <muhammedsubhan692@gmail.com>
2024-12-29 19:04:34 +0500

    changes

--- app/components/adminPageComp/AdminPhoneView.tsx
+++ app/components/adminPageComp/AdminPhoneView.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import { useAppSelector } from "@/app/lib/hooks";
+import { RootState } from "@/app/lib/store/store";
+import React, { useEffect } from "react";
 import { CiShare2 } from "react-icons/ci";
 
 const AdminPhoneView = () => {
+  const socialLinks = useAppSelector(
+    (state: RootState) => state.socialLinks.socialLinks
+  );
+
+  useEffect(() => {
+    console.log("admin phone view", socialLinks);
+  }, [socialLinks]);
   return (
     <>
       <div className="border-l h-screen w-full ">
@@ -22,12 +31,17 @@ const AdminPhoneView = () => {
           >
             <div className="bg-black h-full flex flex-col pt-20 rounded-e-[35px] rounded-s-[35px]">
               <div>
-                <div className="flex flex-col items-center gap-3">
+                <div className="flex flex-col items-center gap-3 mb-5">
                   <div className="rounded-full h-20 w-20 bg-white flex items-center justify-center">
                     <p className="font-bold text-xl">M</p>
                   </div>
                   <div className="text-white font-semibold">
                     <p>@MuhammadSubhan</p>
+                  </div>
+                </div>
+                <div className="px-5">
+                  <div className="border-2 border-red-300 py-5 rounded-xl">
+
                   </div>
                 </div>
               </div>
